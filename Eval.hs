b1620c2190920d0482e2985de6b68ec12eed567b
francisbr <francisbouletrouleau@hotmail.com>
2017-06-03 16:32:51 -0400

    Opperateurs de base fonctionnel
    +, -, * fonctionne!! (la premiere partie de 3.1)

--- Eval.hs
+++ Eval.hs
@@ -121,8 +121,10 @@ eval :: Env -> Exp -> Value
 eval _ (EInt x) = VInt x
 eval env (EVar sym) = lookupVar env sym
 eval env (ELam sym ty ex) = error "ELam not implemented yet"
-eval env (EApp ex1 ex2) = do
-    eval env ex1
+eval env (EApp ex1 ex2) = 
+    case eval env ex1 of
+        VInt x -> eval env ex1
+        VPrim x -> x (eval env ex2)
     --eval env ex2
 --eval _ _ = error "Oups eval ..."
 
